63cef69eab43ca310eaef4a16e6075fe81112184
Florian Schweizer <post.an.flo@gmail.com>
2022-11-09 10:31:05 +0100

    Initial Commit

---
+++ DallETutorial/DallETutorialApp.swift
@@ -0,0 +1,12 @@
+// Created by Florian Schweizer on 09.11.22
+
+import SwiftUI
+
+@main
+struct DallETutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
